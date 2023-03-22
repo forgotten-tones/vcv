ae05a1cf950532209117daabedf961cdd4bdcc3d
Duncan McGreggor <oubiwann@gmail.com>
2023-03-22 12:05:45 -0500

    Early stages in a cxx explore ...

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,5 +1,10 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
+#[cxx::bridge]
+mod ffi {
+    unsafe extern "C++" {
+        include!("plugin.hpp");
 
-include!("bindings.rs");
+        type BlobstoreClient;
+
+        fn new_blobstore_client() -> UniquePtr<BlobstoreClient>;
+    }
+}
