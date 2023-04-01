55608227efc235bae63a50d13aab78a768766328
Duncan McGreggor <oubiwann@gmail.com>
2023-04-01 17:48:21 -0500

    Started working with sexpr lib.

--- modules/modules-dev/src/REPL.cpp
+++ modules/modules-dev/src/REPL.cpp
@@ -1,24 +1,34 @@
+#include <vector>
+#include <string>
+#include <cstdint>
+#include <ostream>
+#include "sexpresso.hpp"
+
 #include "REPL.hpp"
 #include "str.hpp"
 
-namespace tftp {
+namespace tftp
+{
 
-namespace repl {
-std::string version = "0.0.0";
-std::string prompt = "ft-lisp> ";
-std::string welcome = "Welcome to the Forgotten Tones VCV Rack REPL";
+  namespace repl
+  {
+    std::string version = "0.0.0";
+    std::string prompt = "ft-lisp> ";
+    std::string welcome = "Welcome to the Forgotten Tones VCV Rack REPL";
 
-std::string banner() { return welcome + ", v" + version + "\n\n" + prompt; }
+    std::string banner() { return welcome + ", v" + version + "\n\n" + prompt; }
 
-std::string entry(std::string &cmd) {
-  return cmd.substr(prompt.length(), cmd.length() - prompt.length());
-}
-}  // namespace repl
-}  // namespace tftp
+    std::string entry(std::string &cmd)
+    {
+      return cmd.substr(prompt.length(), cmd.length() - prompt.length());
+    }
+  } // namespace repl
+} // namespace tftp
 
 using namespace tftp;
 
-void REPL::reset() {
+void REPL::reset()
+{
   DEBUG("Calling reset ...");
   // TODO: change to "screen" ... and add one for "history"
   text = tftp::repl::banner();
@@ -27,53 +37,66 @@ void REPL::reset() {
 
 void REPL::onReset() { this->reset(); }
 
-void REPL::fromJson(json_t *rootJ) {
+void REPL::fromJson(json_t *rootJ)
+{
   DEBUG("loading data from JSON ...");
   Module::fromJson(rootJ);
   json_t *textJ = json_object_get(rootJ, "text");
-  if (textJ) text = json_string_value(textJ);
+  if (textJ)
+    text = json_string_value(textJ);
   dirty = true;
 }
 
-json_t *REPL::dataToJson() {
+json_t *REPL::dataToJson()
+{
   DEBUG("storing data as JSON ...");
   json_t *rootJ = json_object();
   json_object_set_new(rootJ, "text", json_stringn(text.c_str(), text.size()));
   return rootJ;
 }
 
-void REPL::dataFromJson(json_t *rootJ) {
+void REPL::dataFromJson(json_t *rootJ)
+{
   DEBUG("extracting data from JSON ...");
   json_t *textJ = json_object_get(rootJ, "text");
-  if (textJ) text = json_string_value(textJ);
+  if (textJ)
+    text = json_string_value(textJ);
   dirty = true;
 }
 
-void REPLTextField::step() {
+void REPLTextField::step()
+{
   LedDisplayTextField::step();
-  if (module && module->dirty) {
+  if (module && module->dirty)
+  {
     DEBUG("Performing text update in 'step' ...");
     setText(module->text);
     module->dirty = false;
   }
 }
 
-void REPLTextField::onChange(const ChangeEvent &e) {
-  if (module) {
+void REPLTextField::onChange(const ChangeEvent &e)
+{
+  if (module)
+  {
     std::string newText = getText() + "\n";
     DEBUG("New text: %s", newText.c_str());
     std::vector<std::string> tokens = tftp::str::tokenize(newText, '\n');
     DEBUG("Logging tokens (lines) ...");
-    for (auto line : tokens) DEBUG("\tline: %s", line.c_str());
+    for (auto line : tokens)
+      DEBUG("\tline: %s", line.c_str());
 
     std::string last = tftp::str::lastLine(newText);
     DEBUG("Got text change event (last): %s", last.c_str());
     std::string cmd = tftp::str::penultimateLine(newText);
     DEBUG("Got text change event (cmd): %s", cmd.c_str());
-    if (str::startsWith(last, repl::prompt)) {
+    if (str::startsWith(last, repl::prompt))
+    {
       std::string entry = repl::entry(last);
       DEBUG("Got entry: %s", entry.c_str());
       newText += "You typed: " + entry + "\n" + repl::prompt;
+      auto sexp = sexpresso::parse(entry);
+      DEBUG("Parsed operation: %s", sexp.getChild(1).toString().c_str());
       module->dirty = true;
     }
 
@@ -83,7 +106,8 @@ void REPLTextField::onChange(const ChangeEvent &e) {
 
 void REPLDisplay::draw(const DrawArgs &args) {}
 
-void REPLDisplay::setModule(REPL *module) {
+void REPLDisplay::setModule(REPL *module)
+{
   Vec trPos = Vec(1.2, 0);
   REPLTextField *textField = createWidget<REPLTextField>(trPos);
   textField->box.size = box.size;
@@ -93,8 +117,10 @@ void REPLDisplay::setModule(REPL *module) {
   addChild(textField);
 }
 
-struct REPLWidget : TFTPModuleWidget {
-  REPLWidget(REPL *module) {
+struct REPLWidget : TFTPModuleWidget
+{
+  REPLWidget(REPL *module)
+  {
     setModule(module);
     setPanel("modules-dev/res/REPL");
     addScrews();
